1ec87298fb1fe5254cf15f3d3930ec9665812d4c
levv <1047405013@qq.com>
2019-05-30 14:22:04 +0800

    add webpack-dev-server 自动打包刷新

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 let root = document.getElementById('root');
 let dom = document.createElement('div')
-dom.innerHTML = "hello webpack 666"
+dom.innerHTML = "hello webpack add webpack-dev-server"
 
 root.appendChild(dom);
--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,33 @@ const htmlWebpackPlugin = require('html-webpack-plugin');
 //自动清除dist打包目录及其目录下的所有文件
 const cleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
+
 	// 环境
 	mode: "development",
+
 	// 入口
 	entry: './src/index.js',
+
 	// 出口
 	output: {
 		filename:'main.js',
 		path: path.resolve( __dirname, 'dist')
 	},
+
+	// 自动打包设置
+	devServer: {
+		// 以dist文件为基础启动一个服务器，
+		contentBase: 'dist',
+		// 服务器运行端口
+		port: 9527,
+		// 是否自动打开浏览器
+		open: true
+	},
+
 	// 资源转换打包
 	module: {
 		rules: [
+
 			// url-loader 图片等资源文件打包
 			{
 				test: /\.(png|jpg|gif)$/i,
@@ -26,11 +41,13 @@ module.exports = {
 					}
 				}
 			},
+
 			//css　css-loader style-loader样式文件打包
 			{
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
       },
+
 			//sass-loder sass/scss样式文件打包 
 			//postcss-loader 自动添加厂商前缀
 			{
@@ -49,8 +66,10 @@ module.exports = {
       }
 		]
 	},
+
 	// 插件
 	plugins: [
+	
 		//自动打包html文件插件
 		new htmlWebpackPlugin({
 			template: 'src/index.html'
@@ -59,4 +78,5 @@ module.exports = {
 		//自动清除上一次打包的文件
 		new cleanWebpackPlugin()
 	]
+
 }
